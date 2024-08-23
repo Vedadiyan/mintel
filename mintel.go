bf72113c79ad7a6fbf77ed41bb6f75c8b405a7be
Pouya Vedadiyan <vedadiyan@gmail.com>
2024-08-23 15:51:07 +0800

    template writer

--- mintel.go
+++ mintel.go
@@ -8,21 +8,21 @@ type (
 
 	CreateClient func(Metadata) Telemetry
 
-	KV struct {
+	KeyValue struct {
 		Key   string
 		Value any
 	}
 
 	Logger interface {
-		Debug(...*KV)
-		Info(...*KV)
-		Warning(...*KV)
+		Debug(...*KeyValue)
+		Info(...*KeyValue)
+		Warning(...*KeyValue)
 		Error(error)
 		Flush()
 	}
 
 	Tracer interface {
-		Add(*KV)
+		Add(*KeyValue)
 		Notify()
 		NotifyOne(string)
 		Flush()
@@ -30,7 +30,7 @@ type (
 	}
 
 	Meter interface {
-		Add(*KV)
+		Add(*KeyValue)
 		Notify()
 		NotifyOne(string)
 		Flush()
@@ -59,30 +59,30 @@ var (
 
 func TraceRef[T any](name string, ref *T) TelemetryOpt {
 	return func(t Telemetry) {
-		t.Tracer().Add(KeyValue(name, ref))
+		t.Tracer().Add(KV(name, ref))
 	}
 }
 
 func Trace[T any](name string, v T) TelemetryOpt {
 	return func(t Telemetry) {
-		t.Tracer().Add(KeyValue(name, v))
+		t.Tracer().Add(KV(name, v))
 	}
 }
 
 func MeasureRef[T int | int16 | int32 | int64 | int8 | byte | uint | uint16 | uint32 | uint64 | float32 | float64](name string, ref *T) TelemetryOpt {
 	return func(t Telemetry) {
-		t.Meter().Add(KeyValue(name, ref))
+		t.Meter().Add(KV(name, ref))
 	}
 }
 
 func Measure[T int | int16 | int32 | int64 | int8 | byte | uint | uint16 | uint32 | uint64 | float32 | float64](name string, v T) TelemetryOpt {
 	return func(t Telemetry) {
-		t.Meter().Add(KeyValue(name, v))
+		t.Meter().Add(KV(name, v))
 	}
 }
 
-func KeyValue(key string, value any) *KV {
-	kv := new(KV)
+func KV(key string, value any) *KeyValue {
+	kv := new(KeyValue)
 	kv.Key = key
 	kv.Value = value
 	return kv
